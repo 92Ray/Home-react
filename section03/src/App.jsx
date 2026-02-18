de0d496ec0cb81d8fd46d9ec35944edc304ef3d0
Ray <dnl1992@naver.com>
2026-02-19 08:35:28 +0900

    [ 추가 ] react board CRUD 테스트

--- section03/src/App.jsx
+++ section03/src/App.jsx
@@ -1,93 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 import { useRef } from 'react';
+import Header from './components/Header'
+import Home from './components/Home'
 import { createContext } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import New from './components/New'
+import { Route,Routes } from 'react-router-dom';
+import { useReducer } from 'react';
+import New from './components/New';
 import Edit from './components/Edit';
 import NotFound from './components/NotFound';
 import BoardDetail from './components/BoardDetail';
-import Header from './components/Header';
-import Home from './components/Home';
+
 
 
 const mockData = [
-  {
-    id: 1
-    createdDate : new Date(2026,1,1).getTime();
-    title : "test1",
-    name : "tester1",
-    content : "content1"
-  },
-  {
-    id: 2
-    createdDate : new Date(2026,1,5).getTime();
-    title : "test2",
-    name : "tester2",
-    content : "content2"
-  },
-  {
-    id: 3
-    createdDate : new Date(2026,1,10).getTime();
-    title : "test3",
-    name : "tester3",
-    content : "content3"
-  },
+ {
+ id: 1,
+ createdDate: new Date(2026,1,1).getTime(),
+ title: "test1",
+ name:"tester1",
+ content: "1번 일기 내용",
+ },
+ {
+ id: 2,
+ createdDate: new Date(2026,1,15).getTime(),
+ title: "test2",
+ name:"tester2",
+ content: "2번 일기 내용",
+ },
+ {
+ id: 3,
+ createdDate: new Date(2026,0,25).getTime(),
+ title: "test3",
+ name:"tester3",
+ content: "3번 일기 내용",
+ },
 ];
 
 export const BoardStateContext = createContext();
 export const BoardDispatchContext = createContext();
 //useReducer
 function reducer(state, action) {
-  switch (action.type){
+  switch (action.type) {
     case "CREATE":
       return [action.data, ...state];
-      case "UPDATE":
-        return state.map((item)=>{
-          item.id === action.id ? action.data : item
-        });
-        case "DELETE":
-          return state.filter((item)=>String(item.id) !== String(action.id));
-          default:
-            return state;
+    case "UPDATE":
+      return state.map((item)=>{
+        item.id === action.id ? action.data : item
+      });
+    case "DELETE":
+      return state.filter((item)=>String(item.id) !== String(action.id));
+    default:
+      return state;
   }
 }
 
 function App() {
-  const [count, setCount] = useReducer(useReducer, mockData);
-  const idRef = useRef(4);
-  const onCreate = (title, name, content)=>{
-    const newBoard = {
+const [state, dispatch] = useReducer(reducer, mockData);
+const idRef = useRef(4);
+const onCreate=(title,name,content)=>{
+  const newBoard = {
       id:idRef.current++,
       title,
       name,
       content,
-      createDate
+      createdDate:new Date().getTime()
     }
-    dispatchEvent({type:"UPDATE", data:newBoard});
+    dispatch({type:"CREATE", data:newBoard});
+}
+
+const onUpdate = (id, title, name, content, createdDate)=>{
+    const newBoard = {
+      id,
+      title,
+      name,
+      content,
+      createdDate
+    }
+    dispatch({type:"UPDATE", data:newBoard});
   }
 
-  const onDelete = (id) =>{
-    dispatchEvent({type:"DELETE", id});
+  const onDelete = (id)=>{
+    dispatch({type:"DELETE", id});
   }
+
+
+
+
+
+
 
   return (
     <>
     <BoardStateContext.Provider value={state}>
     <BoardDispatchContext.Provider value={{onCreate, onUpdate, onDelete}}>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home />/}
-        <Route path='/new' element={<New />/}
-        <Route path='/BoardDetail/:id' element={<BoardDetail />/}
-        <Route path='/edit/:no' element={<Edit />/}
-        <Route path='*' element={<NotFound />/}
-      </Routes> 
-      </BoardStateContext.Provider>
-      </BoardDispatchContext.Provider>
+    <Header/>
+     <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/new' element={<New />}/>
+      <Route path='/BoardDetail/:id' element={<BoardDetail />}/>
+      <Route path='/edit/:no' element={<Edit />}/>
+      <Route path='*' element={<NotFound />}/>
+     </Routes>
+    </BoardDispatchContext.Provider>
+    </BoardStateContext.Provider>
     </>
   )
 }
--- section03/src/components/List.jsx
+++ section03/src/components/List.jsx
@@ -2,43 +2,39 @@ import { useNavigate } from "react-router-dom";
 import Board from "./Board";
 
 const List = ({state})=>{
-    
-    const nav = useNavigate
 
-    return<>
-    <div className="board-container">
-        <h2>커뮤니티 게시판</h2>
-        <div className="board-header">
-            <span className="tota-count">전체 글 <b>{state.length}</b>개</span>
-            <button className="write-btn" conClick={()=>nav("/nav")}>글쓰기</button>
-        </div>
+const nav = useNavigate();
 
-        <table className="board-table">
-            <thead>
-                <tr>
-                    <th>번호</th>
-                    <th>제목</th>
-                    <th>작성자</th>
-                    <th>날짜</th>
-                </tr>
-            </thead>
+ return<>
+ <div className="board-container">
+  <h2>커뮤니티 게시판</h2>
+  <div className="board-header">
+    <span className="total-count">전체 글 <b>{state.length}</b>개</span>
+    <button className="write-btn" onClick={()=>nav("/new")}>글쓰기</button>
+  </div>
 
-            <tbody>
-                {state.length > 0 ? (
-                    state.map((item)=><Board key={Number(item.id)} {...item} />)
-                ) : (
-                    <tr>
-                        <td colSpan="4" style={{ padding: "50px" , color: "#999"}}>
-                            등록된 게시글이 없습니다.
-                        </td>
-                    </tr>
-                )}
-
-            </tbody>
-        </table>
-
-    </div>
-    
+  <table className="board-table">
+    <thead>
+      <tr>
+        <th>번호</th>
+        <th>제목</th>
+        <th>작성자</th>
+        <th>날짜</th>
+      </tr>
+    </thead>
+    <tbody>
+  {state.length > 0 ? (
+    state.map((item) => <Board key={Number(item.id)} {...item} />)
+  ) : (
+    <tr>
+      <td colSpan="4" style={{ padding: "50px", color: "#999" }}>
+        등록된 게시글이 없습니다.
+      </td>
+    </tr>
+  )}
+</tbody>
+  </table>
+</div>
     </>
 }
 export default List;
